9bd366044dd129771982206c96a4440129a3a430
taohuadao <dao.taohua@gmail.com>
2023-01-10 19:02:19 +0800

    nothing

---
+++ Src/Loginout.dart
@@ -0,0 +1,27 @@
+// # -*- coding:utf-8 -*-
+// """
+// 登录登出
+// @author: baostock.com
+// @group : baostock.com
+// @contact: baostock@163.com
+// """
+// import zlib
+// import baostock.util.socketutil as sock
+// import baostock.data.resultset as rs
+// import baostock.common.contants as cons
+// import baostock.data.messageheader as msgheader
+// import datetime
+// import baostock.common.context as conx
+
+/*
+    :param user_id:用户ID
+    :param password:密码
+    :param options:可选项，00.5.00版本暂未使用
+    :return: ResultData()
+ */
+void Login({user_id='anonymous', password='123456', options=0}){
+
+}
+
+
+
